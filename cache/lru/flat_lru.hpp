43f4cbb8f26d88ffb46e31011eaa031b91148f58
Denis Pronin <dannftk@yandex.ru>
2024-04-21 17:25:31 +0300

    add a project LRU cache
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

---
+++ cache/lru/flat_lru.hpp
@@ -0,0 +1,278 @@
+#pragma once
+
+#include <cassert>
+#include <cstdint>
+
+#include <algorithm>
+#include <concepts>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <ranges>
+#include <span>
+#include <utility>
+
+/* clang-format off */
+template <
+  typename Key,
+  typename T,
+  typename KeyCompare = std::ranges::less,
+  typename KeyEqual = std::ranges::equal_to
+>
+/* clang-format on */
+  requires std::strict_weak_order<KeyCompare, Key, Key> &&
+           std::equivalence_relation<KeyEqual, Key, Key>
+class flat_lru {
+public:
+  using key_type = Key;
+  using data_type = std::unique_ptr<T[], std::function<void(T[])>>;
+  using key_compare = KeyCompare;
+  using key_equal = KeyEqual;
+
+  static std::unique_ptr<flat_lru> create(uint64_t cache_len,
+                                          uint64_t cache_item_sz);
+
+private:
+  using cache_item_ref_t = uint64_t;
+  using stored_type = struct {
+    data_type data;
+    mutable cache_item_ref_t refs;
+  };
+  using value_type = std::pair<key_type, stored_type>;
+
+  static inline constexpr auto cache_item_to_key_proj =
+      [](value_type const &value) { return value.first; };
+
+public:
+  explicit flat_lru(uint64_t len_max, uint64_t item_sz)
+      : cache_len_max_(len_max), cache_item_sz_(item_sz) {
+    assert(this->len_max() > 0);
+    assert(this->item_sz() > 0);
+
+    cache_.reserve(this->len_max());
+  }
+  ~flat_lru() = default;
+
+  flat_lru(flat_lru const &) = delete;
+  flat_lru &operator=(flat_lru const &) = delete;
+
+  flat_lru(flat_lru &&) = default;
+  flat_lru &operator=(flat_lru &&) = default;
+
+  uint64_t item_sz() const { return cache_item_sz_; }
+  uint64_t len_max() const { return cache_len_max_; }
+
+  std::span<T const> find(key_type const &key) const {
+    if (auto const [index, exact_match] = lower_bound_find(key); exact_match) {
+      touch(index);
+      return data_view(cache_[index]);
+    }
+    return {};
+  }
+
+  std::span<T> find_mutable(key_type const &key) {
+    return const_span_cast(find(key));
+  }
+
+  std::optional<std::pair<key_type, data_type>>
+  update(std::pair<key_type, data_type> value);
+
+  bool exists(key_type const &key) const {
+    return lower_bound_find(key).second;
+  }
+
+  void invalidate(key_type const &key) {
+    if (auto const [index, exact_match]{lower_bound_find(key)}; exact_match)
+      invalidate(cache_[index]);
+  }
+
+  void invalidate_range(std::pair<key_type, key_type> const &range) {
+    for (auto index : range_find(range))
+      invalidate(cache_[index]);
+  }
+
+private:
+  template <typename U>
+  static std::span<U> const_span_cast(std::span<U const> from) {
+    return std::span{const_cast<U *>(from.data()), from.size()};
+  }
+
+  auto data_view(value_type const &value) const {
+    return std::span{value.second.data.get(), item_sz()};
+  }
+
+  void invalidate(value_type &value) {
+    value.second.refs = len_max();
+    value.second.data.reset();
+  }
+
+  bool is_valid(value_type const &value) const {
+    return value.second.refs != len_max();
+  }
+
+  void touch(size_t index) const;
+
+  std::pair<size_t, bool> lower_bound_find(key_type const &key) const;
+
+  auto range_find(std::pair<key_type, key_type> const &range) const;
+
+  size_t evict_index_find() const;
+
+  uint64_t cache_len_max_;
+  uint64_t cache_item_sz_;
+
+  std::vector<value_type> cache_;
+};
+
+template <typename Key, typename T, typename KeyCompare, typename KeyEqual>
+  requires std::strict_weak_order<KeyCompare, Key, Key> &&
+           std::equivalence_relation<KeyEqual, Key, Key>
+auto flat_lru<Key, T, KeyCompare, KeyEqual>::range_find(
+    std::pair<key_type, key_type> const &range) const {
+  assert(range.first < range.second);
+  auto const first{
+      std::ranges::lower_bound(cache_, range.first, key_compare{},
+                               cache_item_to_key_proj),
+  };
+  auto const last{
+      std::ranges::lower_bound(std::ranges::subrange{first, cache_.end()},
+                               range.second, key_compare{},
+                               cache_item_to_key_proj),
+  };
+  return std::views::iota(first - cache_.begin(), last - cache_.begin());
+}
+
+template <typename Key, typename T, typename KeyCompare, typename KeyEqual>
+  requires std::strict_weak_order<KeyCompare, Key, Key> &&
+           std::equivalence_relation<KeyEqual, Key, Key>
+size_t flat_lru<Key, T, KeyCompare, KeyEqual>::evict_index_find() const {
+  auto evict_index{0uz};
+  if (is_valid(cache_[evict_index])) {
+    for (auto index : std::views::iota(1uz, cache_.size())) {
+      if (cache_[evict_index].second.refs < cache_[index].second.refs) {
+        evict_index = index;
+        if (!is_valid(cache_[evict_index]))
+          break;
+      }
+    }
+  }
+  return evict_index;
+}
+
+template <typename Key, typename T, typename KeyCompare, typename KeyEqual>
+  requires std::strict_weak_order<KeyCompare, Key, Key> &&
+           std::equivalence_relation<KeyEqual, Key, Key>
+std::pair<size_t, bool>
+flat_lru<Key, T, KeyCompare, KeyEqual>::lower_bound_find(
+    key_type const &key) const {
+  auto const value_it{
+      std::ranges::lower_bound(cache_, key, key_compare{},
+                               cache_item_to_key_proj),
+  };
+  auto const index{value_it - cache_.begin()};
+  return {
+      static_cast<size_t>(index),
+      cache_.end() != value_it &&
+          key_equal{}(key, cache_item_to_key_proj(*value_it)) &&
+          is_valid(cache_[index]),
+  };
+}
+
+template <typename Key, typename T, typename KeyCompare, typename KeyEqual>
+  requires std::strict_weak_order<KeyCompare, Key, Key> &&
+               std::equivalence_relation<KeyEqual, Key, Key>
+auto flat_lru<Key, T, KeyCompare, KeyEqual>::create(
+    uint64_t cache_len, uint64_t cache_item_sz) -> std::unique_ptr<flat_lru> {
+  auto cache{std::unique_ptr<flat_lru>{}};
+  if (cache_len && cache_item_sz) {
+    cache = std::unique_ptr<flat_lru>{
+        new flat_lru<key_type, T>{
+            cache_len,
+            cache_item_sz,
+        },
+    };
+  }
+  return cache;
+}
+
+template <typename Key, typename T, typename KeyCompare, typename KeyEqual>
+  requires std::strict_weak_order<KeyCompare, Key, Key> &&
+           std::equivalence_relation<KeyEqual, Key, Key>
+void flat_lru<Key, T, KeyCompare, KeyEqual>::touch(size_t index) const {
+  assert(index < cache_.size());
+
+  auto const to_refs{
+      [](value_type const &v) -> cache_item_ref_t & { return v.second.refs; },
+  };
+
+  auto &eo{to_refs(cache_[index])};
+
+  auto make_less_than{
+      [](auto bound) { return [=](auto const &x) { return x < bound; }; },
+  };
+
+  /* clang-format off */
+  for (auto &o :   std::views::all(cache_)
+                 | std::views::transform(to_refs)
+                 | std::views::filter(make_less_than(eo))) {
+    ++o;
+  }
+  /* clang-format on */
+
+  eo = 0;
+}
+
+template <typename Key, typename T, typename KeyCompare, typename KeyEqual>
+  requires std::strict_weak_order<KeyCompare, Key, Key> &&
+               std::equivalence_relation<KeyEqual, Key, Key>
+auto flat_lru<Key, T, KeyCompare, KeyEqual>::update(
+    std::pair<key_type, data_type> value)
+    -> std::optional<std::pair<key_type, data_type>> {
+  auto evicted_value{std::optional<std::pair<key_type, data_type>>{}};
+
+  bool should_evict{true};
+
+  auto [index, exact_match]{lower_bound_find(value.first)};
+  if (!exact_match) {
+    if (!(index < cache_.size()) || is_valid(cache_[index])) {
+      if (!(cache_.size() < len_max())) [[likely]] {
+        auto value_it = cache_.begin() + index;
+
+        if (auto const evict_index{evict_index_find()}; evict_index < index) {
+          value_it = std::rotate(cache_.begin() + evict_index,
+                                 cache_.begin() + evict_index + 1, value_it);
+        } else {
+          std::rotate(value_it, cache_.begin() + evict_index,
+                      cache_.begin() + evict_index + 1);
+        }
+
+        index = value_it - cache_.begin();
+      } else {
+        cache_.emplace(cache_.begin() + index,
+                       std::move_if_noexcept(value.first), stored_type{});
+        invalidate(cache_[index]);
+        cache_[index].second.data = std::move(value.second);
+        should_evict = false;
+      }
+    }
+  }
+
+  if (should_evict) {
+    evicted_value.emplace(
+        std::exchange(cache_[index].first, std::move_if_noexcept(value.first)),
+        std::exchange(cache_[index].second.data, std::move(value.second)));
+  }
+
+  touch(index);
+
+#ifndef NDEBUG
+  if (!exact_match) {
+    assert(
+        std::ranges::is_sorted(cache_, key_compare{}, cache_item_to_key_proj));
+    assert(cache_.end() == std::ranges::adjacent_find(cache_, key_equal{},
+                                                      cache_item_to_key_proj));
+  }
+#endif
+
+  return evicted_value;
+}
---
+++ cache/lru/lru.cpp
@@ -0,0 +1,432 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <cstddef>
+#include <cstdint>
+
+#include <algorithm>
+#include <limits>
+#include <memory>
+#include <random>
+#include <ranges>
+#include <type_traits>
+#include <vector>
+
+#include "flat_lru.hpp"
+
+using namespace testing;
+
+namespace {
+
+using cache = flat_lru<uint64_t, std::byte>;
+
+template <typename T>
+concept is_trivially_copyable = std::is_trivially_copyable_v<T>;
+
+constexpr inline auto make_random_bytes_generator = [] {
+  return [rd = std::random_device{}] mutable {
+    return static_cast<std::byte>(rd());
+  };
+};
+
+inline std::unique_ptr<std::byte[]> make_unique_for_overwrite_bytes(size_t sz) {
+  return std::make_unique_for_overwrite<std::byte[]>(sz);
+}
+
+inline std::unique_ptr<std::byte[]> make_unique_randomized_bytes(size_t sz) {
+  auto buf{std::make_unique_for_overwrite<std::byte[]>(sz)};
+  auto gen = make_random_bytes_generator();
+  std::generate_n(buf.get(), sz, std::ref(gen));
+  return buf;
+}
+
+template <is_trivially_copyable T>
+inline auto duplicate_unique(std::unique_ptr<T[]> const &p, size_t sz) {
+  auto p_dup{std::make_unique_for_overwrite<T[]>(sz)};
+  std::copy_n(p.get(), sz, p_dup.get());
+  return p_dup;
+}
+
+TEST(LRUCache, CreateValid) {
+  constexpr auto kCacheLenMax{1uz};
+  constexpr auto kCacheItemSz{1uz};
+
+  auto cache_not_null{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache_not_null);
+
+  EXPECT_EQ(cache_not_null->len_max(), kCacheLenMax);
+  EXPECT_EQ(cache_not_null->item_sz(), kCacheItemSz);
+}
+
+TEST(LRUCache, CreateInvalid) {
+  constexpr auto kCacheLenMax{1uz};
+  constexpr auto kCacheItemSz{1uz};
+
+  auto cache_nulls{
+      std::array{
+          cache::create(0uz, kCacheItemSz),
+          cache::create(kCacheLenMax, 0uz),
+      },
+  };
+
+  for (auto const &cache_null : cache_nulls)
+    EXPECT_FALSE(cache_null);
+}
+
+TEST(LRUCache, InsertAndFindNonExistent) {
+  constexpr auto kCacheLenMax{32uz};
+  constexpr auto kCacheItemSz{1u};
+
+  auto cache{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache);
+
+  for (auto key : std::views::iota(0uz, kCacheLenMax))
+    cache->update({key, make_unique_for_overwrite_bytes(kCacheItemSz)});
+
+  auto rd{std::random_device{}};
+  auto unexistent_keys_random_gen{
+      std::uniform_int_distribution<uint64_t>{
+          kCacheLenMax,
+          std::numeric_limits<uint64_t>::max(),
+      },
+  };
+
+  for (auto i [[maybe_unused]] : std::views::iota(0, 1024)) {
+    auto const unexistent_key{unexistent_keys_random_gen(rd)};
+    EXPECT_FALSE(cache->exists(unexistent_key));
+    auto const buf{cache->find(unexistent_key)};
+    EXPECT_TRUE(buf.empty());
+  }
+}
+
+TEST(LRUCache, InsertAndFind) {
+  constexpr auto kCacheLenMax{32uz};
+  constexpr auto kCacheItemSz{16uz};
+
+  auto cache{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache);
+
+  auto bufs_pairs{
+      std::vector<std::pair<uint64_t, std::unique_ptr<std::byte[]>>>{
+          cache->len_max(),
+      },
+  };
+  std::ranges::generate(
+      bufs_pairs, [i = 0uz] mutable -> decltype(bufs_pairs)::value_type {
+        return {i++, make_unique_randomized_bytes(kCacheItemSz)};
+      });
+
+  decltype(bufs_pairs) bufs_pairs_dup{cache->len_max()};
+  std::ranges::transform(
+      bufs_pairs, bufs_pairs_dup.begin(),
+      [](auto const &buf_pair) -> decltype(bufs_pairs)::value_type {
+        return {
+            buf_pair.first,
+            duplicate_unique(buf_pair.second, kCacheItemSz),
+        };
+      });
+
+  auto rd{std::random_device{}};
+  std::ranges::shuffle(bufs_pairs, rd);
+
+  for (auto &buf_pair : bufs_pairs) {
+    auto const evicted_value{cache->update(std::move(buf_pair))};
+    EXPECT_FALSE(evicted_value.has_value());
+  }
+
+  for (auto const &[key, buf_dup] : bufs_pairs_dup) {
+    auto const buf{cache->find(key)};
+    EXPECT_THAT(buf, ElementsAreArray(buf_dup.get(), kCacheItemSz));
+  }
+}
+
+TEST(LRUCache, FindMutableAndCheckMutation) {
+  constexpr auto kCacheLenMax{32uz};
+  constexpr auto kCacheItemSz{16uz};
+
+  auto cache{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache);
+
+  auto bufs_pairs{
+      std::vector<std::pair<uint64_t, std::unique_ptr<std::byte[]>>>{
+          cache->len_max(),
+      },
+  };
+  std::ranges::generate(
+      bufs_pairs, [i = 0uz] mutable -> decltype(bufs_pairs)::value_type {
+        return {i++, std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz)};
+      });
+
+  decltype(bufs_pairs) bufs_pairs_dup{cache->len_max()};
+  std::ranges::transform(
+      bufs_pairs, bufs_pairs_dup.begin(),
+      [](auto const &buf_pair) -> decltype(bufs_pairs)::value_type {
+        return {buf_pair.first, std::unique_ptr<std::byte[]>{}};
+      });
+
+  auto rd{std::random_device{}};
+  std::ranges::shuffle(bufs_pairs, rd);
+
+  for (auto &buf_pair : bufs_pairs) {
+    auto const evicted_value{cache->update(std::move(buf_pair))};
+    EXPECT_FALSE(evicted_value.has_value());
+  }
+
+  for (auto &[key, buf_dup] : bufs_pairs_dup) {
+    auto const buf{cache->find_mutable(key)};
+    ASSERT_FALSE(buf.empty());
+    ASSERT_EQ(buf.size(), kCacheItemSz);
+    std::copy_n(make_unique_randomized_bytes(kCacheItemSz).get(), kCacheItemSz,
+                buf.begin());
+    buf_dup = std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz);
+    std::ranges::copy(buf, buf_dup.get());
+  }
+
+  for (auto const &[key, buf_dup] : bufs_pairs_dup) {
+    auto const buf{cache->find(key)};
+    EXPECT_THAT(buf, ElementsAreArray(buf_dup.get(), kCacheItemSz));
+  }
+}
+
+TEST(LRUCache, Invalidate) {
+  constexpr auto kCacheLenMax{32uz};
+  constexpr auto kCacheItemSz{1uz};
+
+  auto cache{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache);
+
+  auto bufs_pairs{
+      std::vector<std::pair<uint64_t, std::unique_ptr<std::byte[]>>>{
+          cache->len_max(),
+      },
+  };
+  std::ranges::generate(
+      bufs_pairs, [i = 0uz] mutable -> decltype(bufs_pairs)::value_type {
+        return {i++, std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz)};
+      });
+
+  auto rd{std::random_device{}};
+  std::ranges::shuffle(bufs_pairs, rd);
+
+  for (auto &[key, buf] : bufs_pairs)
+    cache->update({key, std::move(buf)});
+
+  for (auto keys{std::vector<uint64_t>{}}; !bufs_pairs.empty();) {
+    for (auto key : bufs_pairs | std::views::keys)
+      EXPECT_TRUE(cache->exists(key));
+
+    auto const key{bufs_pairs.back().first};
+    bufs_pairs.pop_back();
+
+    cache->invalidate(key);
+
+    keys.push_back(key);
+    for (auto key_prev : keys | std::views::reverse) {
+      EXPECT_FALSE(cache->exists(key_prev));
+      auto const buf{cache->find(key_prev)};
+      EXPECT_TRUE(buf.empty());
+    }
+  }
+}
+
+TEST(LRUCache, InvalidateRange) {
+  constexpr auto kCacheLenMax{32uz};
+  constexpr auto kCacheItemSz{1uz};
+
+  auto cache{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache);
+
+  auto bufs_pairs{
+      std::vector<std::pair<uint64_t, std::unique_ptr<std::byte[]>>>{
+          cache->len_max(),
+      },
+  };
+  std::ranges::generate(
+      bufs_pairs, [i = 0uz] mutable -> decltype(bufs_pairs)::value_type {
+        return {i++, std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz)};
+      });
+
+  auto rd{std::random_device{}};
+  std::ranges::shuffle(bufs_pairs, rd);
+
+  for (auto &[key, buf] : bufs_pairs)
+    cache->update({key, std::move(buf)});
+
+  for (auto key : bufs_pairs | std::views::keys)
+    EXPECT_TRUE(cache->exists(key));
+
+  cache->invalidate_range({0uz, bufs_pairs.size()});
+
+  for (auto key : bufs_pairs | std::views::keys) {
+    EXPECT_FALSE(cache->exists(key));
+    auto const buf{cache->find(key)};
+    EXPECT_TRUE(buf.empty());
+  }
+}
+
+TEST(LRUCache, EvictValidEntriesInOrder) {
+  constexpr auto kCacheLenMax{32uz};
+  constexpr auto kCacheItemSz{1uz};
+
+  auto cache{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache);
+
+  for (auto key : std::views::iota(0uz, kCacheLenMax)) {
+    auto const evicted_value{
+        cache->update(
+            {key, std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz)}),
+    };
+    ASSERT_FALSE(evicted_value.has_value());
+  }
+
+  for (auto key : std::views::iota(kCacheLenMax, 2 * kCacheLenMax)) {
+    auto const evicted_value{
+        cache->update(
+            {key, std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz)}),
+    };
+    ASSERT_TRUE(evicted_value.has_value());
+    EXPECT_EQ(evicted_value->first, key - kCacheLenMax);
+  }
+}
+
+TEST(LRUCache, EvictInsertInvalidatedEntry) {
+  constexpr auto kCacheLenMax{8uz};
+  constexpr auto kCacheItemSz{1uz};
+  constexpr auto kKeyToVerify{6uz};
+  static_assert(kKeyToVerify < kCacheLenMax);
+
+  auto cache{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache);
+
+  for (auto key : std::views::iota(0uz, kCacheLenMax)) {
+    auto const evicted_value{
+        cache->update({
+            key,
+            std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz),
+        }),
+    };
+    ASSERT_FALSE(evicted_value.has_value());
+  }
+
+  cache->invalidate(kKeyToVerify);
+  ASSERT_FALSE(cache->exists(kKeyToVerify));
+
+  auto const evicted_value{
+      cache->update({kKeyToVerify, std::make_unique_for_overwrite<std::byte[]>(
+                                       kCacheItemSz)}),
+  };
+  ASSERT_TRUE(evicted_value.has_value());
+  EXPECT_EQ(evicted_value->first, kKeyToVerify);
+  EXPECT_FALSE(static_cast<bool>(evicted_value->second));
+}
+
+TEST(LRUCache, EvictInvalidatedEntryInsertOffByOneEntry) {
+  constexpr auto kCacheLenMax{8uz};
+  constexpr auto kCacheItemSz{1uz};
+  constexpr auto kKeyToInvalidate{6uz};
+  static_assert(kKeyToInvalidate < kCacheLenMax);
+  constexpr auto kKeyStride{2uz};
+  static_assert(kKeyStride > 1uz,
+                "key stride must not be 1, there must be a gap in between");
+
+  auto cache{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache);
+
+  for (auto key : std::views::iota(0uz, kCacheLenMax)) {
+    auto const evicted_value{
+        cache->update({
+            kKeyStride * key,
+            std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz),
+        }),
+    };
+    ASSERT_FALSE(evicted_value.has_value());
+  }
+
+  cache->invalidate(kKeyToInvalidate);
+  ASSERT_FALSE(cache->exists(kKeyToInvalidate));
+
+  auto const evicted_value{
+      cache->update({
+          kKeyToInvalidate - 1,
+          std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz),
+      }),
+  };
+  ASSERT_TRUE(evicted_value.has_value());
+  EXPECT_EQ(evicted_value->first, kKeyToInvalidate);
+}
+
+TEST(LRUCache, InvalidateLessInsertEntryEvictInvalidated) {
+  constexpr auto kCacheLenMax{16uz};
+  constexpr auto kCacheItemSz{1uz};
+  constexpr auto kKeyToInsert{7uz};
+  static_assert((kKeyToInsert < kCacheLenMax) && (kKeyToInsert % 2));
+  constexpr auto kKeyToInvalidate{4uz};
+  static_assert((kKeyToInvalidate < kKeyToInsert) &&
+                0 == (kKeyToInvalidate % 2));
+  constexpr auto kKeyStride{2uz};
+  static_assert(0 == (kKeyStride % 2), "key stride must be even");
+
+  auto cache{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache);
+
+  for (auto key : std::views::iota(0uz, kCacheLenMax)) {
+    auto const evicted_value{
+        cache->update({
+            kKeyStride * key,
+            std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz),
+        }),
+    };
+    ASSERT_FALSE(evicted_value.has_value());
+  }
+
+  cache->invalidate(kKeyToInvalidate);
+  ASSERT_FALSE(cache->exists(kKeyToInvalidate));
+
+  auto const evicted_value{
+      cache->update({
+          kKeyToInsert,
+          std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz),
+      }),
+  };
+  ASSERT_TRUE(evicted_value.has_value());
+  EXPECT_EQ(evicted_value->first, kKeyToInvalidate);
+}
+
+TEST(LRUCache, InvalidateGreaterInsertEntryEvictInvalidated) {
+  constexpr auto kCacheLenMax{16uz};
+  constexpr auto kCacheItemSz{1uz};
+  constexpr auto kKeyToInsert{7uz};
+  static_assert((kKeyToInsert < kCacheLenMax) && (kKeyToInsert % 2));
+  constexpr auto kKeyToInvalidate{12uz};
+  static_assert((kKeyToInvalidate > kKeyToInsert) &&
+                0 == (kKeyToInvalidate % 2));
+  constexpr auto kKeyStride{2uz};
+  static_assert(0 == (kKeyStride % 2), "key stride must be even");
+
+  auto cache{cache::create(kCacheLenMax, kCacheItemSz)};
+  ASSERT_TRUE(cache);
+
+  for (auto key : std::views::iota(0uz, kCacheLenMax)) {
+    auto const evicted_value{
+        cache->update({
+            kKeyStride * key,
+            std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz),
+        }),
+    };
+    ASSERT_FALSE(evicted_value.has_value());
+  }
+
+  cache->invalidate(kKeyToInvalidate);
+  ASSERT_FALSE(cache->exists(kKeyToInvalidate));
+
+  auto const evicted_value{
+      cache->update({
+          kKeyToInsert,
+          std::make_unique_for_overwrite<std::byte[]>(kCacheItemSz),
+      }),
+  };
+  ASSERT_TRUE(evicted_value.has_value());
+  EXPECT_EQ(evicted_value->first, kKeyToInvalidate);
+}
+
+} // namespace
