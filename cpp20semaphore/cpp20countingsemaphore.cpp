f933c5f1c3baa3d82b2d7aa36318321c978e76e2
Denis Pronin <dannftk@yandex.ru>
2024-05-06 13:53:09 +0300

    update cpp20countingsemaphore example, made it be more expressive
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- cpp20semaphore/cpp20countingsemaphore.cpp
+++ cpp20semaphore/cpp20countingsemaphore.cpp
@@ -1,33 +1,87 @@
 #include <cassert>
+#include <cstddef>
 
-#include <atomic>
-#include <semaphore>
-#include <thread>
 #include <iostream>
+#include <mutex>
+#include <new>
+#include <numeric>
+#include <ranges>
+#include <semaphore>
+#include <stack>
+#include <thread>
 #include <vector>
 
 constexpr auto kConcurrencyLevelMax{5u};
-constexpr auto kPerThreadCount{1000000u};
+constexpr auto kPerThreadCount{100000000u};
 
-struct thread_shared_info {
-  std::counting_semaphore<kConcurrencyLevelMax> sem{kConcurrencyLevelMax};
-  std::atomic_int32_t a{0};
-  std::atomic_int32_t cnt{0};
+template <typename T, size_t Align>
+#ifdef __clang__
+struct aligned_storage_impl {
+  alignas(Align) std::byte data[sizeof(T)];
+};
+#else
+using aligned_storage_impl = alignas(Align) std::byte[sizeof(T)];
+#endif
+
+template <typename T, size_t Align = alignof(T)> struct aligned_storage {
+  static_assert(!(Align < alignof(T)));
+  using type = aligned_storage_impl<T, Align>;
 };
 
-void f(thread_shared_info &info) {
-  info.sem.acquire();
+template <typename T, size_t Align = alignof(T)>
+using aligned_storage_t = typename aligned_storage<T, Align>::type;
 
-  auto p {info.a.fetch_add(1, std::memory_order_relaxed)};
-  assert(p < kConcurrencyLevelMax);
+#ifdef __cpp_lib_hardware_interference_size
+constexpr size_t hardware_constructive_interference_size =
+    std::hardware_constructive_interference_size;
+#else
+constexpr size_t hardware_constructive_interference_size = 64;
+#endif
 
-  for (int i = 0; i < kPerThreadCount; ++i)
-    info.cnt.fetch_add(1, std::memory_order_relaxed);
+using int_cache_line_aligned =
+    aligned_storage_t<int, hardware_constructive_interference_size>;
 
-  p = info.a.fetch_sub(1, std::memory_order_relaxed);
-  assert(p > 0);
+struct thread_shared_info {
+  thread_shared_info() {
+    for (auto &i : v)
+      items_pool.push(&i);
+  }
 
-  info.sem.release();
+  std::counting_semaphore<kConcurrencyLevelMax> sem{kConcurrencyLevelMax};
+  std::vector<int_cache_line_aligned> v{kConcurrencyLevelMax};
+
+  std::mutex items_pool_lock;
+  std::stack<typename decltype(v)::pointer> items_pool;
+};
+
+void worker(thread_shared_info &shared_data) {
+  shared_data.sem.acquire();
+
+  std::unique_lock l{shared_data.items_pool_lock};
+
+  std::cout << "Thread started ..." << std::endl;
+
+  assert(!shared_data.items_pool.empty());
+
+  auto *p{shared_data.items_pool.top()};
+  shared_data.items_pool.pop();
+
+  l.unlock();
+
+  for (auto &i{reinterpret_cast<int &>(*p)};
+       auto _ : std::views::iota(0u, kPerThreadCount)) {
+    ++i;
+  }
+
+  l.lock();
+
+  shared_data.items_pool.push(p);
+
+  std::cout << "Thread finished ..." << std::endl;
+
+  l.unlock();
+
+  shared_data.sem.release();
 }
 
 int main(int argc, char const *argv[]) {
@@ -35,12 +89,18 @@ int main(int argc, char const *argv[]) {
 
   auto vths{std::vector<std::thread>{std::thread::hardware_concurrency()}};
   for (auto &vth : vths)
-    vth = std::thread{f, std::ref(shared_data)};
+    vth = std::thread{worker, std::ref(shared_data)};
 
   for (auto &vth : vths)
     vth.join();
 
-  auto const r{shared_data.cnt.load(std::memory_order_relaxed)};
+  auto vi{
+      shared_data.v | std::views::transform([](auto &&x) -> int {
+        return reinterpret_cast<int const &>(std::forward<decltype(x)>(x));
+      }),
+  };
+
+  auto const r{std::reduce(vi.begin(), vi.end())};
 
   std::cout << r << std::endl;
 
