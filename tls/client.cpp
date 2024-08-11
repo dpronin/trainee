bcafa333e338764b2997ff47fe107d66b0501867
Denis Pronin <dannftk@yandex.ru>
2024-08-11 20:15:15 +0300

    add simple blocking TLS server and client
    use docs:
    https://docs.openssl.org/master/man7/ossl-guide-tls-server-block/
    https://docs.openssl.org/master/man7/ossl-guide-tls-client-block/
    
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- tls/client.cpp
+++ tls/client.cpp
@@ -1,4 +1,187 @@
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+
+#include <iostream>
+#include <memory>
+#include <new>
+#include <openssl/bio.h>
+#include <openssl/tls1.h>
+#include <openssl/x509_vfy.h>
+#include <print>
+#include <string_view>
+
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#include <openssl/types.h>
+
 int main(int argc, char const *argv[]) {
-  /* code */
+  std::string_view hostport;
+
+  if (argc > 1)
+    hostport = argv[1];
+
+  if (hostport.empty())
+    hostport = "localhost:3333";
+
+  auto ctx{
+      std::unique_ptr<SSL_CTX, void (*)(SSL_CTX *)>{
+          SSL_CTX_new(TLS_client_method()),
+          SSL_CTX_free,
+      },
+  };
+
+  if (!ctx) {
+    ERR_print_errors_fp(stderr);
+    std::println(std::cerr, "Failed to create server SSL_CTX");
+    return EXIT_FAILURE;
+  }
+
+  if (!SSL_CTX_set_min_proto_version(ctx.get(), TLS1_3_VERSION)) {
+    ERR_print_errors_fp(stderr);
+    std::println(std::cerr, "Failed to set the minimum TLS protocol version");
+    return EXIT_FAILURE;
+  }
+
+  SSL_CTX_set_verify(ctx.get(), SSL_VERIFY_PEER, nullptr);
+
+  if (!(SSL_CTX_set_default_verify_paths(ctx.get()) > 0)) {
+    ERR_print_errors_fp(stderr);
+    std::println(std::cerr,
+                 "Failed to set the default trusted certificate store");
+    return EXIT_FAILURE;
+  }
+
+  auto ssl{
+      std::unique_ptr<SSL, void (*)(SSL *)>{SSL_new(ctx.get()), SSL_free},
+  };
+
+  if (!ssl) {
+    ERR_print_errors_fp(stderr);
+    std::println(std::cerr, "Error creating SSL handle for new connection");
+    return EXIT_FAILURE;
+  }
+
+  assert(!hostport.empty());
+
+  auto const hostname{std::string{hostport.substr(0, hostport.find(':'))}};
+  auto const port{
+      hostport.substr(std::min(hostname.size() + 1, hostport.size())),
+  };
+
+  addrinfo hints{};
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+
+  auto services{
+      std::unique_ptr<addrinfo, void (*)(addrinfo *)>{
+          nullptr,
+          freeaddrinfo,
+      },
+  };
+
+  if (addrinfo * result{nullptr};
+      getaddrinfo(hostname.data(), port.data(), &hints, &result) == 0) {
+    services.reset(result);
+  } else {
+    perror("Failed getaddrinfo() call");
+    return EXIT_FAILURE;
+  }
+
+  assert(static_cast<bool>(services));
+
+  auto sock{
+      std::unique_ptr<int const, void (*)(int const *pfd)>{
+          nullptr,
+          +[](int const *pfd) {
+            close(*pfd);
+            delete pfd;
+          },
+      },
+  };
+
+  for (auto *p_srv = services.get(); p_srv; p_srv = p_srv->ai_next) {
+    int const fd{socket(AF_INET, SOCK_STREAM, 0)};
+    if (fd < 0) {
+      perror("couldn't open a new socket");
+      return EXIT_FAILURE;
+    }
+
+    if (connect(fd, p_srv->ai_addr, p_srv->ai_addrlen) < 0) {
+      close(fd);
+      continue;
+    }
+
+    sock.reset(new (std::nothrow) int{fd});
+    assert(static_cast<bool>(sock));
+
+    break;
+  }
+
+  if (!static_cast<bool>(sock)) {
+    std::println("couldn't connect to {}", hostport);
+    return EXIT_FAILURE;
+  }
+
+  auto bio{
+      std::unique_ptr<BIO, int (*)(BIO *)>{
+          BIO_new(BIO_s_socket()),
+          BIO_free,
+      },
+  };
+  assert(static_cast<bool>(bio));
+
+  BIO_set_fd(bio.get(), *sock, BIO_NOCLOSE);
+
+  SSL_set_bio(ssl.get(), bio.get(), bio.get());
+  bio.release();
+
+  if (!SSL_set_tlsext_host_name(ssl.get(), hostname.data())) {
+    ERR_print_errors_fp(stderr);
+    std::println(std::cerr, "Failed to set the SNI hostname '{}'", hostname);
+    return EXIT_FAILURE;
+  }
+
+  if (!SSL_set1_host(ssl.get(), hostname.data())) {
+    ERR_print_errors_fp(stderr);
+    std::println(std::cerr,
+                 "Failed to set the certificate verification hostname '{}'",
+                 hostname);
+    return EXIT_FAILURE;
+  }
+
+  if (SSL_connect(ssl.get()) < 1) {
+    ERR_print_errors_fp(stderr);
+    std::println(std::cerr, "Failed to connect to the server '{}'", hostname);
+    if (SSL_get_verify_result(ssl.get()) != X509_V_OK) {
+      std::println(
+          std::cerr, "Verify error: %s\n",
+          X509_verify_cert_error_string(SSL_get_verify_result(ssl.get())));
+    }
+    return EXIT_FAILURE;
+  }
+
+  using namespace std::string_view_literals;
+
+  auto msg{"Hello, world"sv};
+
+  size_t nwritten{0};
+  SSL_write_ex(ssl.get(), msg.data(), msg.size(), &nwritten);
+
+  assert(msg.size() == nwritten);
+
+  std::array<char, 1024> buf;
+  size_t nread{0};
+  SSL_read_ex(ssl.get(), buf.data(), buf.size(), &nread);
+
+  msg = {buf.data(), nread};
+
+  std::println(std::cout, "echo from '{}': '{}'", hostname, msg);
+
   return 0;
 }
--- tls/server.cpp
+++ tls/server.cpp
@@ -1,24 +1,27 @@
 #include <cassert>
+#include <cstdio>
 #include <cstdlib>
 
 #include <iostream>
 #include <memory>
 #include <print>
+#include <string_view>
 
 #include <openssl/bio.h>
 #include <openssl/crypto.h>
 #include <openssl/err.h>
 #include <openssl/prov_ssl.h>
 #include <openssl/ssl.h>
+#include <openssl/types.h>
 
 int main(int argc, char const *argv[]) {
-  std::string_view hostport;
+  std::string_view port;
 
   if (argc > 1)
-    hostport = argv[1];
+    port = argv[1];
 
-  if (hostport.empty())
-    hostport = "localhost:8000";
+  if (port.empty())
+    port = "3333";
 
   auto ctx{
       std::unique_ptr<SSL_CTX, void (*)(SSL_CTX *)>{
@@ -29,13 +32,13 @@ int main(int argc, char const *argv[]) {
 
   if (!ctx) {
     ERR_print_errors_fp(stderr);
-    println(std::cerr, "Failed to create server SSL_CTX");
+    std::println(std::cerr, "Failed to create server SSL_CTX");
     return EXIT_FAILURE;
   }
 
   if (!SSL_CTX_set_min_proto_version(ctx.get(), TLS1_3_VERSION)) {
     ERR_print_errors_fp(stderr);
-    println(std::cerr, "Failed to set the minimum TLS protocol version");
+    std::println(std::cerr, "Failed to set the minimum TLS protocol version");
     return EXIT_FAILURE;
   }
 
@@ -48,28 +51,31 @@ int main(int argc, char const *argv[]) {
 
   if (!(SSL_CTX_use_certificate_chain_file(ctx.get(), "server.pem") > 0)) {
     ERR_print_errors_fp(stderr);
-    println(std::cerr, "Failed to load the server certificate chain file");
+    std::println(std::cerr, "Failed to load the server certificate chain file");
     return EXIT_FAILURE;
   }
 
   if (!(SSL_CTX_use_PrivateKey_file(ctx.get(), "server.key", SSL_FILETYPE_PEM) >
         0)) {
     ERR_print_errors_fp(stderr);
-    println(std::cerr, "Error loading the server private key file, possible "
-                       "key/cert mismatch???");
+    std::println(std::cerr,
+                 "Error loading the server private key file, possible "
+                 "key/cert mismatch???");
     return EXIT_FAILURE;
   }
 
   SSL_CTX_set_verify(ctx.get(), SSL_VERIFY_NONE, nullptr);
 
-  assert(!hostport.empty());
+  assert(!port.empty());
 
   auto acceptor_bio{
       std::unique_ptr<BIO, int (*)(BIO *)>{
-          BIO_new_accept(hostport.data()),
+          BIO_new_accept(port.data()),
           BIO_free,
       },
   };
+
+  ERR_print_errors_fp(stderr);
 
   if (!acceptor_bio) {
     ERR_print_errors_fp(stderr);
@@ -80,55 +86,62 @@ int main(int argc, char const *argv[]) {
   BIO_set_bind_mode(acceptor_bio.get(), BIO_BIND_REUSEADDR);
   if (!(BIO_do_accept(acceptor_bio.get()) > 0)) {
     ERR_print_errors_fp(stderr);
-    println(std::cerr, "Error setting up acceptor socket");
+    std::println(std::cerr, "Error setting up acceptor socket");
     return EXIT_FAILURE;
   }
 
-  ERR_clear_error();
+  while (true) {
+    ERR_clear_error();
 
-  auto client_bio{
-      std::unique_ptr<BIO, int (*)(BIO *)>{
-          BIO_pop(acceptor_bio.get()),
-          BIO_free,
-      },
-  };
-
-  println(std::cout, "New client connection accepted");
-
-  auto ssl{
-      std::unique_ptr<SSL, void (*)(SSL *)>{SSL_new(ctx.get()), SSL_free},
-  };
-
-  if (!ssl) {
-    ERR_print_errors_fp(stderr);
-    std::println(std::cerr, "Error creating SSL handle for new connection");
-    return EXIT_FAILURE;
-  }
-
-  SSL_set_bio(ssl.get(), client_bio.get(), client_bio.get());
-
-  if (!(SSL_accept(ssl.get()) > 0)) {
-    ERR_print_errors_fp(stderr);
-    std::println(std::cerr, "Error performing SSL handshake with client");
-    return EXIT_FAILURE;
-  }
-
-  size_t total{0};
-
-  std::array<char, 1024> buf;
-  for (size_t nread{0}; SSL_read_ex(ssl.get(), buf.data(), buf.size(), &nread);
-       nread = 0) {
-    size_t nwritten{0};
-    if (SSL_write_ex(ssl.get(), buf.data(), nread, &nwritten) &&
-        nwritten == nread) {
-      total += nwritten;
+    if (!(BIO_do_accept(acceptor_bio.get()) > 0)) {
       continue;
     }
-    std::println(std::cerr, "Error echoing client input");
-    break;
-  }
 
-  std::println(std::cerr, "Client connection closed, {} bytes sent", total);
+    auto bio{
+        std::unique_ptr<BIO, int (*)(BIO *)>{
+            BIO_pop(acceptor_bio.get()),
+            BIO_free,
+        },
+    };
+
+    std::println(std::cout, "New client connection accepted");
+
+    auto ssl{
+        std::unique_ptr<SSL, void (*)(SSL *)>{SSL_new(ctx.get()), SSL_free},
+    };
+
+    if (!ssl) {
+      ERR_print_errors_fp(stderr);
+      std::println(std::cerr, "Error creating SSL handle for new connection");
+      continue;
+    }
+
+    SSL_set_bio(ssl.get(), bio.get(), bio.get());
+    bio.release();
+
+    if (!(SSL_accept(ssl.get()) > 0)) {
+      ERR_print_errors_fp(stderr);
+      std::println(std::cerr, "Error performing SSL handshake with client");
+      continue;
+    }
+
+    size_t total{0};
+
+    std::array<char, 1024> buf;
+    for (size_t nread{0};
+         SSL_read_ex(ssl.get(), buf.data(), buf.size(), &nread); nread = 0) {
+      if (size_t nwritten{0};
+          SSL_write_ex(ssl.get(), buf.data(), nread, &nwritten) &&
+          nwritten == nread) {
+        total += nwritten;
+        continue;
+      }
+      std::println(std::cerr, "Error echoing client input");
+      break;
+    }
+
+    std::println(std::cerr, "Client connection closed, {} bytes sent", total);
+  }
 
   return EXIT_SUCCESS;
 }
