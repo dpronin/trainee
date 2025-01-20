936c8b0a878590e7958597096ca6988953055e1f
Denis Pronin <dannftk@yandex.ru>
2025-01-20 15:44:23 +0300

    send aligned_storage.hpp out in xroost library
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- aligned_storage/aligned_storage.cpp
+++ aligned_storage/aligned_storage.cpp
@@ -14,24 +14,9 @@
 
 #include <print>
 
-template <typename T, size_t Align>
-#ifdef __clang__
-struct aligned_storage_impl {
-  alignas(Align) std::byte data[sizeof(T)];
-};
-#else
-using aligned_storage_impl = alignas(Align) std::byte[sizeof(T)];
-#endif
+#include <xroost/utility/aligned_storage.hpp>
 
-template <typename T, size_t Align = alignof(T)> struct aligned_storage {
-  static_assert(!(Align < alignof(T)));
-  using type = aligned_storage_impl<T, Align>;
-};
-
-template <typename T, size_t Align = alignof(T)>
-using aligned_storage_t = typename aligned_storage<T, Align>::type;
-
-using int_aligned_8 = aligned_storage_t<int, 8>;
+using int_aligned_8 = xroost::aligned_storage_t<int, 8>;
 
 int main(int argc, char const *argv[]) {
   int_aligned_8 i;
